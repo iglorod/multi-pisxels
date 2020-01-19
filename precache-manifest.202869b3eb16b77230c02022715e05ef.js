f4733e7c852751a4edbb54924aed08ebdfa83b7b
igor29 <igmatviytsev@gmail.com>
2020-01-19 19:03:03 +0200

    Updates

---
+++ precache-manifest.202869b3eb16b77230c02022715e05ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd35e1e338cb314ff1da8e074930475",
+    "url": "/multi.pixels/index.html"
+  },
+  {
+    "revision": "39e090bb2e99a322b10e",
+    "url": "/multi.pixels/static/css/main.15ba0e6f.chunk.css"
+  },
+  {
+    "revision": "f47b7c295df284f49138",
+    "url": "/multi.pixels/static/js/2.0b2c86de.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/multi.pixels/static/js/2.0b2c86de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39e090bb2e99a322b10e",
+    "url": "/multi.pixels/static/js/main.a34add99.chunk.js"
+  },
+  {
+    "revision": "9ef2dcf05a3079d6ee9f",
+    "url": "/multi.pixels/static/js/runtime-main.cf27e579.js"
+  },
+  {
+    "revision": "b13e3e9856e7d3288a58462f075daaf1",
+    "url": "/multi.pixels/static/media/New.b13e3e98.png"
+  },
+  {
+    "revision": "92c38688b90fe4d15dd56985fb967c5e",
+    "url": "/multi.pixels/static/media/noposter.92c38688.jpg"
+  },
+  {
+    "revision": "011cd56511a2314c7408c019eeaa5cf4",
+    "url": "/multi.pixels/static/media/pngguru1.011cd565.png"
+  },
+  {
+    "revision": "35302ca002f2159af27840dbed627da5",
+    "url": "/multi.pixels/static/media/sorry.35302ca0.png"
+  }
+]);
